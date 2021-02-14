3791bfb62efd7af39f2c0e0f42e48f0bb84bd6b7
N <75640078+N12345678987654321@users.noreply.github.com>
2021-02-14 21:57:25 +0400

    vpn website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages';
+import SigninPage from './pages/signin';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Helmet>
+        <title>NeutrinoVPN</title>
+      </Helmet> 
+      <Switch>
+        <Route path="/NeutrinoVPN" component={Home} exact />
+        <Route path="/NeutrinoVPN/signin" component={SigninPage} exact />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { animateScroll as scroll } from 'react-scroll';
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink,  } from './FooterElements';
+
+const Footer = () => {
+    const  toggleHome = () => {
+        scroll.scrollToTop();
+    }
+
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                                <FooterLink to="/NeutrinoVPN/signin">How it Works</FooterLink>
+                                <FooterLink to="/NeutrinoVPN/signin">Documentation</FooterLink>
+                                <FooterLink to="/NeutrinoVPN/signin">Careers</FooterLink>
+                                <FooterLink to="/NeutrinoVPN/signin">Newsroom</FooterLink>
+                                <FooterLink to="/NeutrinoVPN/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                                <FooterLink to="/NeutrinoVPN/signin">Contact</FooterLink>
+                                <FooterLink to="/NeutrinoVPN/signin">Support</FooterLink>
+                                <FooterLink to="/NeutrinoVPN/signin">Destinations</FooterLink>
+                                <FooterLink to="/NeutrinoVPN/signin">Sponsorships</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/NeutrinoVPN' onClick={toggleHome}>NeutrinoVPN</SocialLogo>
+                        <WebsiteRights>NeutrinoVPN © {new Date().getFullYear()} All Rights Reserved.</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="//www.facebook.com" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.instagram.com" target="_blank" aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.youtube.com" target="_blank" aria-label="Youtube">
+                                <FaYoutube />
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.twitter.com" target="_blank" aria-label="Twitter">
+                                <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.linkedin.com" target="_blank" aria-label="Linkedin">
+                                <FaLinkedin />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer;
